fffbf7b7aa9cd680c791f8f9d39e11e66007e379
Milen Tecle <m_tecle@hotmail.com>
2023-12-17 19:38:10 +0000

    Display the tasks added in chronical order

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -21,3 +21,7 @@ M.Datepicker.init(datepicker, {
 let selects = document.querySelectorAll('select');
 M.FormSelect.init(selects);
 
+// collapsible initialization
+let collapsibles = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsibles);
+
