6d269e3d78c350e7e34bf08c1f12896d80f785a3
Milen Tecle <m_tecle@hotmail.com>
2023-12-17 15:35:03 +0000

    Add the tasks function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
+
+//Modal
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+});
+
+// datepicker initialization
+let datepicker= document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+});
+
+// Select initialization
+let selects = document.querySelectorAll('select');
+M.FormSelect.init(selects);
+
